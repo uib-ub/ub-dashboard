e3fac11b9e8937a985f3f0c6ce9416499c4d8648
Tarje Lavik <tarje.lavik@gmail.com>
2022-01-13 13:14:48 +0100

    Add list and count on languages

--- web/pages/graph/index.js
+++ web/pages/graph/index.js
@@ -13,44 +13,51 @@ const GraphComponentWithoutSSR = dynamic(
 )
 
 const myQuery = groq`{
-  "nodes": *[_type in ['Service', "Software", "Language"]] {
-    "id": _id,
-    label,
-    "size": 12 + (count(*[references(^._id)]) * 4),
-    "style": {
-      _type == 'Service' && !defined(timespan.endOfTheEnd) => {
-        "color": "#DB1D16"
-      },
-      _type == 'Service' && defined(timespan.endOfTheEnd) => {
-        "color": "gray"
-      },
-      _type == 'Software' => {
-        "color": "#0B7EDB"
-      },
-      _type == 'Language' => {
-        "color": "#DB8E16"
+  "graph": {
+    "nodes": *[_type in ['Service', "Software", "Language"]] {
+      "id": _id,
+      label,
+      "size": 12 + (count(*[references(^._id)]) * 4),
+      "style": {
+        _type == 'Service' && !defined(timespan.endOfTheEnd) => {
+          "color": "#DB1D16"
+        },
+        _type == 'Service' && defined(timespan.endOfTheEnd) => {
+          "color": "gray"
+        },
+        _type == 'Software' => {
+          "color": "#0B7EDB"
+        },
+        _type == 'Language' => {
+          "color": "#DB8E16"
+        },
+      }
+    },
+    "edges": *[_type in ['Service', 'Software'] && defined(uses)] {
+      uses[] {
+        "id": ^._id + _ref,
+        "source": ^._id,
+        "target": _ref,
+        "label": "uses",
       }
     }
   },
-  "edges": *[_type in ['Service', 'Software'] && defined(uses)] {
-     uses[] {
-      "id": ^._id + _ref,
-      "source": ^._id,
-      "target": _ref,
-      "label": "uses"
-    }
+  "languages": *[_type == 'Language'] {
+    "id": _id,
+    label,
+    "count": count(*[references(^._id)]),
   }
 }`;
 
 export const getStaticProps = async ({ preview = false }) => {
-  let graph = await getClient(preview).fetch(myQuery)
-  graph.edges = flattenDeep(graph.edges.map(edge => { return ([...edge.uses.map(use => { return { ...use } })]) }))
-  graph = cleanDeep(graph)
+  let data = await getClient(preview).fetch(myQuery)
+  data.graph.edges = flattenDeep(data.graph.edges.map(edge => { return ([...edge.uses.map(use => { return { ...use } })]) }))
+  data = cleanDeep(data)
 
   return {
     props: {
       preview,
-      data: graph,
+      data: data,
     },
   }
 }
@@ -63,9 +70,14 @@ export default function TechGraph({ data }) {
         <Heading>
           Tek-graf
         </Heading>
-        <Box className="graphpaper-background" maxH={"70vh"} overflow={"hidden"} my="5">
+        <Box className="graphpaper-background" maxH={"50vh"} overflow={"hidden"} my="5">
           <Box></Box>
-          <GraphComponentWithoutSSR edges={data.edges} nodes={data.nodes} />
+          <GraphComponentWithoutSSR edges={data.graph.edges} nodes={data.graph.nodes} />
+        </Box>
+        <Box>
+          {data?.languages && data?.languages.map(lang => (
+            <Heading as="h2" size={"lg"}>{lang.label} ({lang.count})</Heading>
+          ))}
         </Box>
 
         {/* <PortableText blocks={data.content} /> */}
