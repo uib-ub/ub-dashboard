e238e8b5b6d2837c8b0e4d277eb2057eedce08b6
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-04-28 11:06:18 +0200

    Update sanity and add some desk sctructure icons

--- studio/src/deskStructure/index.js
+++ studio/src/deskStructure/index.js
@@ -2,6 +2,28 @@
 import S from '@sanity/desk-tool/structure-builder'
 import DocumentsPane from 'sanity-plugin-documents-pane'
 import { createDeskHierarchy } from '@sanity/hierarchical-document-list'
+import { GoDatabase, GoFileCode, GoLightBulb, GoPackage, GoPerson, GoProject } from 'react-icons/go'
+import { GrCloudSoftware } from 'react-icons/gr'
+import { BiCodeAlt, BiUser } from 'react-icons/bi'
+import { MdDesignServices, MdOutlineEvent } from 'react-icons/md'
+import { AiOutlineApi } from 'react-icons/ai'
+import { GiTeamIdea } from 'react-icons/gi'
+
+const icons = {
+  project: GoProject,
+  product: GoPackage,
+  software: GrCloudSoftware,
+  sourcecode: GoFileCode,
+  programminglanguage: BiCodeAlt,
+  service: MdDesignServices,
+  dataset: GoDatabase,
+  accesspoint: AiOutlineApi,
+  actor: GoPerson,
+  group: BiUser,
+  team: GiTeamIdea,
+  autority: GoLightBulb,
+  event: MdOutlineEvent
+}
 
 export const getDefaultDocumentNode = () => {
   // Give all documents the JSON preview, 
@@ -24,10 +46,63 @@ export default () =>
   S.list()
     .title('Innhold')
     .items([
-      S.documentTypeListItem('Project'),
-      S.documentTypeListItem('Product'),
+      S.documentTypeListItem('Project')
+        .icon(icons.project),
+      S.documentTypeListItem('Actor')
+        .icon(icons.actor),
+      S.documentTypeListItem('Team')
+        .icon(icons.team),
+      S.listItem()
+        .title('Grupper')
+        .icon(icons.group)
+        .child(
+          S.list()
+            .title('Grupper')
+            .items([
+              S.listItem()
+                .title('Alle grupper')
+                .child(
+                  S.documentTypeList('Group')
+                ),
+              S.listItem()
+                .title('Grupper etter type')
+                .icon(icons.group)
+                .child(
+                  // List out all categories
+                  S.documentList('GroupType')
+                    .schemaType('GroupType')
+                    .title('Grupper etter type')
+                    .filter('_type == "GroupType"')
+                    .child((groupTypeId) =>
+                      // List out project documents where the _id for the selected
+                      // category appear as a _ref in the project’s categories array
+                      S.documentList()
+                        .schemaType('Group')
+                        .title('Gruppe')
+                        .filter('_type == "Group" && $groupTypeId in hasType[]._ref')
+                        .params({ groupTypeId })
+                    ),
+                ),
+              createDeskHierarchy({
+                title: 'Oranisasjonshierarki',
+                documentId: 'org-hierarchy',
+                referenceTo: ['Group', 'Actor'],
+                // ❓ Optional: provide filters and/or parameters for narrowing which documents can be added
+                /* referenceOptions: {
+                  filter: 'status in $acceptedStatuses',
+                  filterParams: {
+                    acceptedStatuses: ['published', 'approved']
+                  }
+                } */
+              })
+            ])
+        ),
+      S.divider(),
+      S.documentTypeListItem('Product')
+        .icon(icons.product),
       S.listItem()
         .title('Programvare')
+        .icon(icons.software)
         .child(
           S.list()
             // Sets a title for our new list
@@ -82,6 +157,7 @@ export default () =>
             ])),
       S.listItem()
         .title('Tjenester')
+        .icon(icons.service)
         .child(
           S.list()
             // Sets a title for our new list
@@ -110,39 +186,14 @@ export default () =>
                   S.documentTypeList('SoftwareCuratingService')
                 ),
             ])),
-      S.documentTypeListItem('Dataset'),
-      S.documentTypeListItem('AccessPoint'),
-      S.divider(),
-      S.documentTypeListItem('Actor'),
-      S.documentTypeListItem('Team'),
-      S.listItem()
-        .title('Grupper')
-        .child(
-          S.list()
-            .title('Grupper')
-            .items([
-              S.listItem()
-                .title('Alle grupper')
-                .child(
-                  S.documentTypeList('Group')
-                ),
-              createDeskHierarchy({
-                title: 'Oranisasjonshierarki',
-                documentId: 'org-hierarchy',
-                referenceTo: ['Group', 'Actor'],
-                // ❓ Optional: provide filters and/or parameters for narrowing which documents can be added
-                /* referenceOptions: {
-                  filter: 'status in $acceptedStatuses',
-                  filterParams: {
-                    acceptedStatuses: ['published', 'approved']
-                  }
-                } */
-              })
-            ])
-        ),
+      S.documentTypeListItem('Dataset')
+        .icon(icons.dataset),
+      S.documentTypeListItem('AccessPoint')
+        .icon(icons.accesspoint),
       S.divider(),
       S.listItem()
         .title('Autoriteter')
+        .icon(icons.autority)
         .child(
           S.list()
             // Sets a title for our new list
@@ -224,6 +275,7 @@ export default () =>
       S.divider(),
       S.listItem()
         .title('Hendelser og aktiviteter')
+        .icon(icons.event)
         .child(
           S.list()
             // Sets a title for our new list
