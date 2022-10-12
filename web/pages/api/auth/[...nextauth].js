ae21abf296158510756737145b4db6e5f8168244
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-10-12 13:50:28 +0200

    clean up auth api file

--- web/pages/api/auth/[...nextauth].js
+++ web/pages/api/auth/[...nextauth].js
@@ -1,39 +1,7 @@
 import NextAuth from "next-auth"
 
-const AUTH_ENDPOINT = process.env.DATAPORTEN_AUTH_ENDPOINT
-const SCOPES = 'openid groups-org profile userid email userinfo-photo'
-
 export const authOptions = {
-  /* callbacks: {
-    session: async (session, data) => {
-      const { accessToken, email_verified, picture, sub } = data;
-      const username = session.user.email.split('@')[0];
-      const user = {
-        fullName: session.user.name,
-        username,
-        email: session.user.email,
-        emailVerified: email_verified,
-        id: sub,
-        image: picture,
-        accessToken,
-        expiresAt: Date.parse(session.expires),
-      };
-      return user;
-    },
-    jwt: async (token, _, account, profile) => {
-      if (account && account.accessToken) {
-        token.accessToken = account.accessToken;
-      }
-      return Promise.resolve({ ...token, ...profile });
-    },
-  }, */
-  // Configure one or more authentication providers
   providers: [
-    /* GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }), */
-
     {
       id: 'dataporten',
       name: 'DATAPORTEN',
@@ -43,7 +11,6 @@ export const authOptions = {
       clientId: process.env.DATAPORTEN_ID,
       clientSecret: process.env.DATAPORTEN_SECRET,
       profile: (profile) => {
-        console.log(profile)
         return {
           ...profile,
           id: profile.sub,
@@ -52,20 +19,6 @@ export const authOptions = {
           image: profile.picture,
         };
       },
-      /* authorization: {
-        params: { scope: SCOPES },
-        //url: `${AUTH_ENDPOINT}/oauth/authorization`
-      },
-      //version: '2.0',
-      debug: true, */
-      /* 
-      token: {
-        url: `${AUTH_ENDPOINT}/oauth/token`
-      },
-      params: {
-        grant_type: 'authorization_code',
-      },
-      userinfo: `${AUTH_ENDPOINT}/userinfo`, */
     },
   ]
 }
